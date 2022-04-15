8919174b2eddb2cad81d335beea58a5447d63e8a
Douglas B <guod1977@gmail.com>
2022-04-14 19:20:30 -0700

    data update

--- src/components/ExpandedCard.jsx
+++ src/components/ExpandedCard.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import xSolid from '../images/x-solid.png'
+import React from "react";
+import { motion } from "framer-motion";
+import xSolid from "../images/x-solid.png";
 
 function ExpandedCard(props) {
   return (
-    <motion.div 
+    <motion.div
       className="expandedCard"
-      layoutId='expandableCard'
-      transition={{duration:.5}}
+      layoutId="expandableCard"
+      transition={{ duration: 0.5 }}
       layout
-      >
-        <button className='btn-close' onClick={props.customClickEvent} >
-            <img className='btn-image' src={xSolid} />
-        </button>
-        <motion.div 
-      layoutId="techStack"
-      className="expanded-card-tech">
+    >
+      <button className="btn-close" onClick={props.customClickEvent}>
+        <img className="btn-image" src={xSolid} />
+      </button>
+      <motion.div layoutId="techStack" className="expanded-card-tech">
         {props.html && <p className="html">html</p>}
         {props.css && <p className="css">css</p>}
         {props.javascript && <p className="javascript">javascript</p>}
@@ -26,29 +24,43 @@ function ExpandedCard(props) {
         {props.express && <p className="express">express</p>}
         {props.mongoDB && <p className="mongoDB">mongoDB</p>}
       </motion.div>
-      <motion.h2 className="expanded-card-title"
-      layoutId="title">{props.title}</motion.h2>
+      <motion.h2 className="expanded-card-title" layoutId="title">
+        {props.title}
+      </motion.h2>
       <div className="main-content-container">
         <div className="description">{props.description}</div>
         <div className="card-image-container">
-          <motion.img className="card-image" src={props.thumbnail}/>
+          <motion.img className="card-image" src={props.thumbnail} />
         </div>
       </div>
       <div className="links">
-                  <div className="site-link-container">
-                    {props.url && <a href={props.url} target="_blank" className="live-site-link">Live site</a>}
-                    {props.metamask && <div className="metamask">
-                      <p className="metamask-text">*requires MetaMask</p>
-                      <a href="https://metamask.io/" target="blank">
-                        <img width="64" src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg"/>
-                      </a>
-                    </div>}
-                  </div>
-                  {props.code && <a href={props.code} target="_blank" className="code-link">Code</a>}
-                  {props.video && <a>video</a>}
+        <div className="site-link-container">
+          {props.url && (
+            <a href={props.url} target="_blank" className="live-site-link">
+              Live site
+            </a>
+          )}
+          {props.metamask && (
+            <div className="metamask">
+              <p className="metamask-text">*requires MetaMask</p>
+              <a href="https://metamask.io/" target="blank">
+                <img
+                  width="64"
+                  src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg"
+                />
+              </a>
+            </div>
+          )}
+        </div>
+        {props.code && (
+          <a href={props.code} target="_blank" className="code-link">
+            Code
+          </a>
+        )}
+        {props.video && <a>video</a>}
       </div>
     </motion.div>
-  )
+  );
 }
 
-export { ExpandedCard }
+export { ExpandedCard };
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,7 +4,7 @@ import React from "react";
 const projects=[
     {
         "title": "Solana NFT Gallery",
-        "description": "DESCRIPTION GOES HERE",
+        "description": "The Solana NFT Viewer project was created to display all NFTs held in any wallet on the Solana blockchain. You are able to enter a Solana wallet address in the search bar and once the data is returned from the blockchain, the NFTs held in that wallet will be displayed. Wallets are searched directly from the blockchain (not third party or centralized servers) ensuring up to date and accurate information. You can then 'favorite' any NFT which will reorder the entire collection and display the favorited NFTs first. Clicking on any NFT will take you to Solscan.io to display more info about that particular NFT.\nIf a NFT is part of a collection, the title will be edited and the NFT number in the collection will be displayed under the favorite icon.\nIncluded is a button to generate a random Solana address in case you don't have access to a wallet address.",
         "thumbnail": require("../images/nft-viewer.png"),
         "html": true,
         "css": true,
@@ -12,8 +12,8 @@ const projects=[
         "react": true,
         "solidity": false,
         "rust":false,
-        "url": "https://web3vote.netlify.app/",
-        "code": "https://github.com/Texopolis/voteapp",
+        "url": "https://solananftviewer.netlify.app",
+        "code": "https://github.com/Texopolis/solanaNFTviewer",
         "video": false
     },
     {
@@ -29,11 +29,11 @@ const projects=[
         "metamask": true,
         "url": "https://web3vote.netlify.app/",
         "code": "https://github.com/Texopolis/voteapp",
-        "video": true
+        "video": false,
     },
     {
         "title": "CLI Rust Calculator",
-        "description": "DESCRIPTION GOES HERE",
+        "description": "This is a simple command line interface calculator written entirely in Rust. This is my first project using Rust exclusively and it represents a recent meaningful step in my journey to write software professionally.",
         "thumbnail": require('../images/rust_calc_thumb.png'),
         "html": false,
         "css": false,
@@ -41,13 +41,14 @@ const projects=[
         "react": false,
         "solidity": false,
         "rust":true,
-        "url": "https://web3vote.netlify.app/",
-        "code": "https://github.com/Texopolis/voteapp",
+        "metamask":false,
+        "url": false,
+        "code": "https://github.com/Texopolis/calculator",
         "video": false
     },
     {
-        "title": "Teacher Notes App",
-        "description": "DESCRIPTION GOES HERE",
+        "title": "Notes App",
+        "description": "This application makes a call to an API where student information is kept. The returning information is styled and displayed with search functionality added. Each student 'card' is expandable and will display additional details. A user is also able to add a note to each entry and then search by entries.",
         "thumbnail": require("../images/teacher_notes_thumb.png"),
         "html": true,
         "css": true,
@@ -55,13 +56,13 @@ const projects=[
         "react": true,
         "solidity": false,
         "rust":false,
-        "url": "https://web3vote.netlify.app/",
-        "code": "https://github.com/Texopolis/voteapp",
+        "url": "https://class-notes-app.netlify.app/",
+        "code": "https://github.com/Texopolis/notesApp",
         "video": false
     },
     {
         "title": "Web3 Random Password Generator",
-        "description": "This app was created using React and was submitted to the Fall 2021 Chainlink Hackathon. It's a simple random password generator but it's randomness is derived from the Chainlink VRF. It calls a smart contract written in Solidity and deployed on the Kovan testnet. By using the Chainlink VRF the randomness of the password can be verified by a blockchain transaction. This verifiable randomness has tremendous application use cases in game logic and anywhere users are affected from a random event. The project provided 4 main challenges: 1. create a responsive app with React 2.write and deploy a smart contract with Solidity 3. use Ethers.io and Metamask to communicate between the Ethereum blockchain and the app 4. call the Chainlink VRF and return the result to the app. I was successful in meeting all my initial goals. Additional instructions and links to Kovan test ether in Github readme",
+        "description": "This app was created using React and was submitted to the Fall 2021 Chainlink Hackathon. It's a simple random password generator but it's randomness is derived from the Chainlink VRF. It calls a smart contract written in Solidity and deployed on the Kovan testnet. By using the Chainlink VRF the randomness of the password can be verified by a blockchain transaction. This verifiable randomness has tremendous application use cases in game logic and anywhere users are affected from a random event.\n The project provided 4 main challenges: 1. create a responsive app with React 2.write and deploy a smart contract with Solidity 3. use Ethers.io and Metamask to communicate between the Ethereum blockchain and the app 4. call the Chainlink VRF and return the result to the app. I was successful in meeting all my initial goals. Additional instructions and links to Kovan test ether in Github readme",
         "thumbnail": require('../images/web3thumb.png'),
         "html": true,
         "css": true,
@@ -70,9 +71,9 @@ const projects=[
         "solidity": true,
         "rust":false,
         "metamask": true,
-        "url": "https://web3vote.netlify.app/",
-        "code": "https://github.com/Texopolis/voteapp",
-        "video": true
+        "url": "https://web3passgenerator.netlify.app",
+        "code": "https://github.com/Texopolis/passgen",
+        "video": false
     },
     {
         "title": "Trout Counter",
@@ -84,8 +85,8 @@ const projects=[
         "react": false,
         "solidity": false,
         "rust":false,
-        "url": "https://web3vote.netlify.app/",
-        "code": "https://github.com/Texopolis/voteapp",
+        "url": "https://mcfarlandsprings.com/",
+        "code": "https://github.com/Texopolis/McFarland-Springs-counter-component",
         "video": false
     },
     {
@@ -98,13 +99,13 @@ const projects=[
         "react": false,
         "solidity": false,
         "rust":false,
-        "url": "https://web3vote.netlify.app/",
-        "code": "https://github.com/Texopolis/voteapp",
+        "url": "https://texopolis.github.io/apiDashboard/",
+        "code": "https://github.com/Texopolis/apiDashboard",
         "video": false
     },
     {
         "title": "In Progress...",
-        "description": "DESCRIPTION GOES HERE",
+        "description": "This is a full-stack CRUD travel application in development that allows a user to create, read, edit and delete a reservation from a database. Each reservation entry includes a location name, date of arrival and departure, type of accommodation, number of guests and the price.",
         "thumbnail": require('../images/crud_app.png'),
         "html": true,
         "css": true,
